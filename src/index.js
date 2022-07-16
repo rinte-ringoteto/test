5d21bd1096c8736619c512e84c0925cdf670901b
rinte-ringoteto <109403092+rinte-ringoteto@users.noreply.github.com>
2022-07-16 17:17:36 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -69,16 +69,76 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
 
-//アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です。"));
+// //アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name:"ジャケ",
+//   age:28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ['ジャケ', 28];
+// //  const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// //  console.log(message3);
+
+//  const [name, age] = myProfile
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+//  console.log(message4);
+
+/**
+ * デフォルト値
+ */
+// const sayHello = (name = "ゲスト") =>{
+//   return console.log(`こんにちは${name}さん！`)
+// }
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+//配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0],arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
